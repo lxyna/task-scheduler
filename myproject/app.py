f63b2649bf3c7d5c9df27b9582a087b8991728dd
smendoza07 <mendozasaul07@gmail.com>
2022-07-03 13:39:45 -0700

    Deleted main.css and edited app.py

--- myproject/app.py
+++ myproject/app.py
@@ -1,32 +1,27 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-class Task(db.Model):
+class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
-    description = db.Column(db.String(200))
     complete = db.Column(db.Boolean)
-
-    #def __repr__(self):
-        #return '<Task %r>' % self.id
 
 @app.route('/')
 def index():
-    task_list = Task.query()
-    print(task_list)
+    todo_list = Todo.query.all()
+    print(todo_list)
     return render_template('base.html')
 
 if __name__ == "__main__":
     db.create_all()
 
-    new_task = Task(title="Build Task", description="idk", complete=False)
-    db.session.add(new_task)
-    db.session.commit()
+   # new_todo = Todo(complete = False)
+  #  db.session.add(new_todo)
+   # db.session.commit()
 
     app.run(debug=True)
