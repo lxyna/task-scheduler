0f45aca65a4f49a9baf6a05c53c7bbda41cbf93d
smendoza07 <mendozasaul07@gmail.com>
2022-07-03 10:00:59 -0700

    Using SQLAlchemy for db

--- myproject/app.py
+++ myproject/app.py
@@ -1,22 +1,32 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-class Todo(db.Model):
+class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable = False)
-    date_priority = db.Column(db.String(20), nullable = False)
+    title = db.Column(db.String(100))
+    description = db.Column(db.String(200))
+    complete = db.Column(db.Boolean)
 
-    def __repr__(self):
-        return '<Task %r>' % self.id
+    #def __repr__(self):
+        #return '<Task %r>' % self.id
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/')
 def index():
-    return render_template('index.html')
+    task_list = Task.query()
+    print(task_list)
+    return render_template('base.html')
 
 if __name__ == "__main__":
+    db.create_all()
+
+    new_task = Task(title="Build Task", description="idk", complete=False)
+    db.session.add(new_task)
+    db.session.commit()
+
     app.run(debug=True)
