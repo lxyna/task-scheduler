ac9f2ad641f128f589aa88616cf7b3f4b7db00a2
smendoza07 <mendozasaul07@gmail.com>
2022-07-03 21:16:04 -0700

    Deleted additional files and updated database

--- taskApp/app.py
+++ taskApp/app.py
@@ -6,22 +6,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100))
-
-
-@app.route('/')
-def index():
-    task_list = Task.query.all()
-    print(task_list)
-    return str(task_list)
-
 if __name__ == "__main__":
     db.create_all()
-
-    #new_task = Task()
-    #db.session.add(new_task)
-    #db.session.commit()
-
     app.run(debug=True)
