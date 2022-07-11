1e54bcfa3934e37c22b48ae4a08fca4a559aa54a
Adil Mohiuddin <amohi9046@gmail.com>
2022-07-11 14:39:02 -0700

    add basic view of tasks

---
+++ frontend/components/TaskList.js
@@ -0,0 +1,33 @@
+import { Group } from '@mantine/core';
+import { useEffect } from 'react';
+import { atom, useRecoilState } from 'recoil';
+import TaskCard from './TaskCard';
+
+const taskListState = atom({
+    key: 'task_list',
+    default: [],
+});
+
+
+export default function TaskList() {
+    const [taskList, setTaskList] = useRecoilState(taskListState);
+
+    useEffect(() => {
+        fetch('http://localhost:3001/tasks', {
+            method: 'GET'
+        })
+            .then(res => res.json())
+            .then(tasks => setTaskList(tasks));
+
+    }, [setTaskList]);
+
+    return (
+        <Group direction='column' align='center' sx={{ margin: '.5em' }}>
+            {Array.from(taskList)
+                .sort((a, b) => b.priority - a.priority)
+                .map(task =>
+                    <TaskCard key={task.id} task={task}/>
+                )}
+        </Group>
+    );
+}
