ee95e3a796571935a5f9702be0913a6a49bd3bd1
lxyna <leynadiep@gmail.com>
2022-07-11 13:34:41 -0700

    Added task class to app.py

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,22 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+# @app.route('/')
+# def index():
+#     task_list = Task.query.all()
+#     print(task_list)
+#     return str(task_list)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    priority = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(300))
+    # classification = db.Column()
+
 if __name__ == "__main__": #creates database by running python app.py
     db.create_all()
+    # new_task = Task()
+    # db.session.add(new_task)
+    # db.session.commit()
     app.run(debug=True)
