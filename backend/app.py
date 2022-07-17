e7235d09caa4d883e90b5fbf859c4f62b01e73df
smendoza07 <mendozasaul07@gmail.com>
2022-07-17 14:54:46 -0700

    Fixes #13, Receive and pass data to new task from json

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, json, jsonify, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,11 +6,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# @app.route('/')
-# def index():
-#     task_list = Task.query.all()
-#     print(task_list)
-#     return str(task_list)
+@app.route('/')
+def index():
+    task_list = Task.query.all()
+    print(task_list)
+    return str(task_list)
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -24,16 +24,19 @@ class Task(db.Model):
 
 @app.route('/new', methods=['POST'])
 def new():
-    task1=Task( title="task1",  priority = 1, description = "description1" )
-          #new_task = Task()
-     db.session.add(task1)
-     db.session.commit()
-     
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        data = request.json
+        new_task = Task(title=(data['title']), priority=data['priority'], description=data['description'])
+        db.session.add(new_task)
+        db.session.commit()
+        return str(new_task)
+    else:
+        return 'Content-Type not supported!'
 
 
 
 if __name__ == "__main__": #creates database by running python app.py
     db.create_all()
-    
     app.run(debug=True)
 
