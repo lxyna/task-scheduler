6c8b32687523139e0b3b2523f70ac66a8a34df49
jiandarugar <jiandarugar@gmail.com>
2022-07-21 14:50:54 -0700

    Deleted messages

--- backend/app.py
+++ backend/app.py
@@ -46,23 +46,4 @@ def new():
 if name == "main": #creates database by running python app.py
     db.create_all()
     app.run(debug=True)
-oh
-and before you push
-also update the requirements.txt
-click==8.1.3
-colorama==0.4.5
-Flask==2.1.2
-flask-marshmallow==0.14.0
-Flask-SQLAlchemy==2.5.1
-greenlet==1.1.2
-itsdangerous==2.1.2
-Jinja2==3.1.2
-MarkupSafe==2.1.1
-marshmallow==3.17.0
-marshmallow-sqlalchemy==0.28.1
-packaging==21.3
-pyparsing==3.0.9
-six==1.16.0
-SQLAlchemy==1.4.39
-Werkzeug==2.1.2
 
